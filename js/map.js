2112a9417dd0db02e2a988818fe7f394b311a65f
biio-front <bohwajung@naver.com>
2020-11-04 12:07:14 +0900

    store information data move to json and fetch

--- js/map.js
+++ js/map.js
@@ -15,74 +15,62 @@ const positions = [
   {
     content: "하겐다즈 서초점",
     latlng: new naver.maps.LatLng(37.489598, 127.006941),
-    class_name: ".store_name .seocho",
-    store_detail: ".store_detail .secho_detail",
+    class_name: ".store_name .seocho"
   },
   {
     content: "하겐다즈 여의도점",
     latlng: new naver.maps.LatLng(37.521054, 126.923947),
-    class_name: ".store_name .yeoido",
-    store_detail: ".store_detail .yeoido_detail",
+    class_name: ".store_name .yeoido"
   },
   {
     content: "하겐다즈 교대점",
     latlng: new naver.maps.LatLng(37.498056, 127.013213),
-    class_name: ".store_name .kyodae",
-    store_detail: ".store_detail .kyodae_detail",
+    class_name: ".store_name .kyodae"
   },
   {
     content: "하겐다즈 도곡점",
     latlng: new naver.maps.LatLng(37.488576, 127.054282),
-    class_name: ".store_name .dogok",
-    store_detail: ".store_detail .dogok_detail",
+    class_name: ".store_name .dogok"
   },
   {
     content: "하겐다즈 사당 DV점",
     latlng: new naver.maps.LatLng(37.479972, 126.971564),
-    class_name: ".store_name .sadang",
-    store_detail: ".store_detail .sadang_detail",
+    class_name: ".store_name .sadang"
   },
   {
     content: "하겐다즈 분당미금점",
     latlng: new naver.maps.LatLng(37.35032, 127.106684),
-    class_name: ".store_name .bundang",
-    store_detail: ".store_detail .bundang_detail",
+    class_name: ".store_name .bundang"
   },
   {
     content: "하겐다즈 판교점",
     latlng: new naver.maps.LatLng(37.394237, 127.111994),
-    class_name: ".store_name .pankyo",
-    store_detail: ".store_detail .pankyo_detail",
+    class_name: ".store_name .pankyo"
   },
   {
     content: "하겐다즈 아주대병원점",
     latlng: new naver.maps.LatLng(37.264523, 127.061705),
-    class_name: ".store_name .ajouUniversityHospital",
-    store_detail: ".store_detail .ajouUniversityHospital_detail",
+    class_name: ".store_name .ajouUniversityHospital"
   },
   {
     content: "하겐다즈 대구신세계점",
     latlng: new naver.maps.LatLng(35.877789, 128.628572),
-    class_name: ".store_name .shinsegae",
-    store_detail: ".store_detail .shinsegae_detail",
+    class_name: ".store_name .shinsegae"
   },
   {
     content: "하겐다즈 양산점",
     latlng: new naver.maps.LatLng(35.329789, 129.012891),
-    class_name: ".store_name .yangsan",
-    store_detail: ".store_detail .yangsan_detail",
+    class_name: ".store_name .yangsan"
   },
   {
     content: "하겐다즈 마린시티점",
     latlng: new naver.maps.LatLng(35.155081, 129.142281),
-    class_name: ".store_name .marineCity",
-    store_detail: ".store_detail .marineCity_detail",
+    class_name: ".store_name .marineCity"
   },
   {
     content: "하겐다즈 용호자이점",
     latlng: new naver.maps.LatLng(35.131837, 129.110663),
-    class_name: ".store_name .yonghoXii",
-    store_detail: ".store_detail .yonghoXii_detail",
+    class_name: ".store_name .yonghoXii"
   },
 ];
 
@@ -96,10 +84,10 @@ for (let i = 0; i < positions.length; i++) {
 
 // 매장 리스트 클릭 시 지도 이동 및 내용 표시
 for (let i = 0; i < positions.length; i++) {
-  $(positions[i].class_name).click(e => {
-    e.preventDefault();
-    map.setCenter(positions[i].latlng);
-
-    $(positions[i].store_detail).addClass("on").siblings().removeClass("on");
-  });
+  document.querySelector(positions[i].class_name)
+  .addEventListener('click', e => {
+      e.preventDefault();
+      map.setCenter(positions[i].latlng);
+    }
+  );
 }
---
+++ js/store.js
@@ -0,0 +1,54 @@
+// Json에서 매장정보 가져오기
+function loadStore() {
+  return fetch("../data/storeInfo.json")
+    .then(response => response.json())
+    .then(json => json.storeInfo);
+}
+
+// HTML에 들어갈 매장정보
+function createHTML(storeInfo) {
+  return `<div class="${storeInfo.class}">
+            <h4>하겐다즈 ${storeInfo.place}</h4>
+            <div>
+                <h5>주소</h5>
+                <p>${storeInfo.address}</p>
+                <h5>영업시간</h5>
+                ${
+                    storeInfo.openingHours
+                    .map(time => `<p>${time}</p>`).join('')
+                }
+            </div>
+        </div>`;
+}
+
+function displayStoreInfo(storeInfo) {
+  const infoDiv = document.querySelector(".store_info");
+  infoDiv.innerHTML = storeInfo
+    .map(storeInfo => createHTML(storeInfo))
+    .join("");
+}
+
+function storeInfoON(e) {
+    const target = e.target.dataset.value;
+    const information = document.querySelectorAll('.store_info > div');
+
+    e.preventDefault();
+    if(target == null) return;
+    [].forEach.call(information, info => {
+        if(info.classList.contains(target)) {
+            info.classList.add('on');
+        } else {
+            info.classList.remove('on');
+        }
+    })
+}
+
+function init() {
+    const sotreList = document.querySelector('.store_name');
+
+    loadStore().then(storeInfo => {
+      displayStoreInfo(storeInfo);
+    });
+    sotreList.addEventListener('click', e => storeInfoON(e));
+}
+init();
